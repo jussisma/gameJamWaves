df89a4b4fd9ccb048de696cd04a52c812b7029d7
Yohan <yohan50@users.noreply.github.com>
2025-12-03 22:29:47 +0100

    Start page

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var ui: Control = $InGameUI
+@onready var ui: Control = $UIinGame
 
 func _ready() -> void:
 	var selected_weapons: Array[String] = ["Glock P80", "Revolver Colt 45", "AK47", "Bazooka M20"]
